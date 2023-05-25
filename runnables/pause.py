86278435b9f3e408dbdd980fdf6e3a9d17aed916
Bas <BlasixCorp@gmail.com>
2023-05-25 14:17:19 +0200

    added some more music control commands

--- runnables/pause.py
+++ runnables/pause.py
@@ -2,12 +2,12 @@ import discord;
 import playerManager;
 
 def command(bot):
-    @bot.tree.command(name="stop", description="Stop playing")
+    @bot.tree.command(name="pause", description="Pause the music")
     async def run(interaction: discord.Interaction):
         try:
             if playerManager.voiceConnection.is_playing():
-                playerManager.voiceConnection.stop()
-                await interaction.response.send_message('Stopped playing')
+                playerManager.voiceConnection.pause()
+                await interaction.response.send_message('Paused')
             else:
                 await interaction.response.send_message('Nothing is playing')
         except Exception as e:
